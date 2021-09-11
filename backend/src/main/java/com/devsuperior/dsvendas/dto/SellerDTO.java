27be63061b66191b426b12bd72277a2f3e09c96a
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2021-09-11 00:22:46 -0400

    Camadas diversas classe Seller

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class SellerDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String name;
+
+	public SellerDTO(Seller entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
+
+}
