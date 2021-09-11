da596ccc8099a45dc3838d12a073788ec87ef233
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2021-09-11 00:02:35 -0400

    Modelos e preenchimento bd

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dsvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_sellers")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Seller {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	@Setter(AccessLevel.NONE)
+	private List<Sale> sales = new ArrayList<>();
+
+}
